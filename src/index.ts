bdf822052c3061dd20fdc6ec6b3debb43aa7bd0f
Kevin <Kevin@email.com>
2022-03-16 22:33:20 -0400

    0.1

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { fetchRiverReadingData } from "./services/river_reading_services";
 
-const 
+const generateRiverDataFiles
 
-fetchRiverReadingData('66945000' , new Date (2022, 2, 16) , new Date (202
+fetchRiverReadingData('66945000' , new Date (2022, 2, 16) , new Date (2022, 2, 16))
