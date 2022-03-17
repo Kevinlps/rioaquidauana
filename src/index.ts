1705bfc145fdc6b7e65b01213b1dc73cb2972b81
Kevin <Kevin@email.com>
2022-03-16 22:31:12 -0400

    rio Aquidauana

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { fetchRiverReadingData } from "./services/river_reading_services";
+
+const 
+
+fetchRiverReadingData('66945000' , new Date (2022, 2, 16) , new Date (202
---
+++ src/utils/files_utils.ts
@@ -0,0 +1,12 @@
+import { writeFileSync } from "fs";
+import { RiverReading } from "../models/RiverReading";
+
+const saveToCsvFile = (readings : RiverReading[], fileName:string )=>{
+    let csvContent = 'COD. ESTACAO,NIVEL,VAZAO,CHUVA,DATA/HORA\n' 
+    readings.forEach(r => {
+        const line = `${r.stationCode},${r.level},${r.flow},${r.rain},${r.dateTime}\n`
+        csvContent = `${csvContent}${line}`
+       
+    })
+     writeFileSync(fileName, csvContent)
+}
