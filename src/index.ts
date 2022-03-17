d2b1531d7c11c28d20585a8858c6f91c4d79d27b
Kevin <Kevin@email.com>
2022-03-16 22:37:14 -0400

    0.2

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
+import { RiverReading } from "./models/RiverReading";
 import { fetchRiverReadingData } from "./services/river_reading_services";
+import { saveToCsvFile } from "./utils/files_utils";
 
-const generateRiverDataFiles
+const generateRiverDataFiles =async (
+    stationCode:string,
+    initialDate:Date,
+    finalDate:Date
+) => {
+    const readings: RiverReading[] = await fetchRiverReadingData(
+        stationCode,
+        initialDate,
+        finalDate
+
+    )
+    saveToCsvFile(readings, 'leituras.csv')
+    
+}
 
 fetchRiverReadingData('66945000' , new Date (2022, 2, 16) , new Date (2022, 2, 16))
--- src/utils/files_utils.ts
+++ src/utils/files_utils.ts
@@ -1,7 +1,7 @@
 import { writeFileSync } from "fs";
 import { RiverReading } from "../models/RiverReading";
 
-const saveToCsvFile = (readings : RiverReading[], fileName:string )=>{
+export const saveToCsvFile = (readings : RiverReading[], fileName:string )=>{
     let csvContent = 'COD. ESTACAO,NIVEL,VAZAO,CHUVA,DATA/HORA\n' 
     readings.forEach(r => {
         const line = `${r.stationCode},${r.level},${r.flow},${r.rain},${r.dateTime}\n`
